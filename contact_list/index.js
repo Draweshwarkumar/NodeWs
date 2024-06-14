5904d7289b94892a157a8e9d15c9564efd301b56
Draweshwarkumar <shivamjha22948@gmail.com>
2024-06-14 16:09:01 +0530

    Template engine

--- contact_list/index.js
+++ contact_list/index.js
@@ -1,13 +1,45 @@
 const express= require('express');
+const path = require('path');
+const { title } = require('process');
 const port = 8000;
 
 const app = express();
 
 
-app.get('/profile',function(req,res){
-    console.log(req);
-    res.send('<h1>it is running! or is it?</h1>');
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+var contactlist = [
+    {
+        name:"shivam",
+        phone:"7654160094"
+    },
+    {
+        name: "pankaj",
+        phone: "1234567789"
+    },
+    {
+        name:"Ritik",
+        phone: "797932120"
+    },
+    {
+        name:"Draweshwar",
+        phone:"9128525710"
+    }
+]
+
+app.get('/',function(req,res){
+    // console.log(__dirname);
+    // res.send('<h1>it is running! or is it?</h1>');
+    return res.render('contact',{
+        title: "Contact list",
+        contact_list: contactlist
+    });
 });
+
+app.post('/new-contact',function(res,req){
+    return res.redirect('/practice');
+})
 
 
 
